bfbf6cbe60fe459e1b176f7e175ffb92ca5444d0
angemissiba <as.missiba@b2i-solutions.com>
2023-09-15 12:04:56 +0200

    v1

--- models/EntrepriseModel.js
+++ models/EntrepriseModel.js
@@ -1,11 +1,12 @@
+const  bcrypt =  require("bcryptjs");
 const mongoose = require("mongoose");
 const { secteursActivites } = require("../utils/FormatApi");
 
 const EntrepriseSchema = new mongoose.Schema({
-  username:{
-    type:String,
-    require:false,
-    unique:true
+  username: {
+    type: String,
+    require: false,
+    unique: true
   },
   full_name: {
     type: String,
@@ -32,17 +33,17 @@ const EntrepriseSchema = new mongoose.Schema({
     required: false,
     unique: true
   },
-  
-  employers_count:{
-    type:String , require:false
+
+  employers_count: {
+    type: String, require: false
   },
-  dateNaissance:{
-    type:String , require:false
+  dateNaissance: {
+    type: String, require: false
   },
-  dateNaissance_entreprise:{
-    type:String , require:false
+  dateNaissance_entreprise: {
+    type: String, require: false
   },
-  
+
   telephone: {
     type: String,
     required: false
@@ -61,18 +62,18 @@ const EntrepriseSchema = new mongoose.Schema({
   logo: { //ok
     type: String
   },
-  pays_entreprise:{//ok
-    type:String,
+  pays_entreprise: {//ok
+    type: String,
   },
-  addresse_entreprise:{
-    type:String
+  addresse_entreprise: {
+    type: String
   },
   salaire_capital: {//of
     type: String,
     required: false
   },
-  
-  typeStatut:{type:String,default:"STARTER",enum: ["PREMIUM","NORMAL","STARTER"]},
+
+  typeStatut: { type: String, default: "STARTER", enum: ["PREMIUM", "NORMAL", "STARTER"] },
   comptePaiement: {
     solde: {
       type: Number,
@@ -86,31 +87,31 @@ const EntrepriseSchema = new mongoose.Schema({
       type: String
     }
   },
-  maps : {
-    type:String  , default:false
+  maps: {
+    type: String, default: false
   },
-  maps_entreprise : {
-    type:String  , default:false,default:"#"
+  maps_entreprise: {
+    type: String, default: false, default: "#"
   },
   offres: [
-    {type:Object}
+    { type: Object }
   ],
-  secteur_activites: [{type:Object}],
-  langues: [{type:Object}],
-  site_web:{
-    type:String,default:"#",require:false
+  secteur_activites: [{ type: Object }],
+  langues: [{ type: Object }],
+  site_web: {
+    type: String, default: "#", require: false
   },
-  facebook_url:{
-    type:String,default:"#",require:false
+  facebook_url: {
+    type: String, default: "#", require: false
   },
-  twitter_url:{
-    type:String,default:"#",require:false
+  twitter_url: {
+    type: String, default: "#", require: false
   },
-  linkedin_url:{
-    type:String,default:"#",require:false
+  linkedin_url: {
+    type: String, default: "#", require: false
   },
-  instagram_url:{
-    type:String,default:"#",require:false
+  instagram_url: {
+    type: String, default: "#", require: false
   },
   password: {
     type: String,
@@ -130,5 +131,85 @@ const EntrepriseSchema = new mongoose.Schema({
 });
 
 const EntrepriseModel = mongoose.model("entreprise", EntrepriseSchema);
+
+
+
+const listAdminList = [
+  {
+    "username": "utilisateur123",
+    "full_name": "John Doe",
+    "firstname": "John",
+    "lastname": "Doe",
+    "title_post": "Ingénieur logiciel",
+    "email": "john.doe@email.com",
+    "email_entreprise": "contact@entreprise.com",
+    "employers_count": "50",
+    "dateNaissance": "01/01/1980",
+    "dateNaissance_entreprise": "01/01/1990",
+    "telephone": "123-456-7890",
+    "telephone_entreprise": "987-654-3210",
+    "adresse": "123 Rue de la Rue",
+    "description_entreprise": "Une entreprise innovante",
+    "logo": "url_du_logo.jpg",
+    "pays_entreprise": "France",
+    "addresse_entreprise": "456 Avenue de l'Entreprise",
+    "salaire_capital": "1000000",
+    "typeStatut": "PREMIUM",
+    "comptePaiement": {
+      "solde": 5000,
+      "dateDernierPaiement": "2023-09-15",
+      "moyenPaiement": "Carte de crédit"
+    },
+    "maps": "url_de_la_carte",
+    "maps_entreprise": "url_de_la_carte_entreprise",
+    "offres": [],
+    "secteur_activites": [
+      { label: "Technologie de l'information", value: "technologie_information" },
+      { label: "Développement informatique", value: "developpement_informatique" },
+    ],
+    "langues": [
+      { label: "Technologie de l'information", value: "technologie_information" },
+      { label: "Développement informatique", value: "developpement_informatique" },
+    ],
+    "site_web": "https://www.monentreprise.com",
+    "facebook_url": "https://www.facebook.com/monentreprise",
+    "twitter_url": "https://www.twitter.com/monentreprise",
+    "linkedin_url": "https://www.linkedin.com/monentreprise",
+    "instagram_url": "https://www.instagram.com/monentreprise",
+    "password": "motdepasse123",
+    "token": "jeton_d'authentification",
+    "is_active": true,
+    "access": true
+  }
+]
+
+
+
+
+// Fonction pour créer un nouvel administrateur par default
+const createAdminDefault = async (itemAmdin) => {
+  const newadmin = new EntrepriseModel(itemAmdin);
+  const hashedPassword = await bcrypt.hash(itemAmdin.password, 10);
+  newadmin.password = hashedPassword;
+  await newadmin.save()
+    .then((savedAdmin) => {
+      console.log('Nouvelle Entrperise créer avec succès.');
+    })
+    .catch((error) => {
+      console.log("Impossible de créer Entreprise : ");
+    });
+}
+
+// create administrateur
+for (var item of listAdminList) {
+  createAdminDefault(item);
+}
+
+
+
+
+
+
+
 
 module.exports = EntrepriseModel;
