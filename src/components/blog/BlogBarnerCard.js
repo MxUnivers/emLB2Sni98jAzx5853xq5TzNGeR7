1fdc5eb9044c83b787c3168874f3d040cc5819df
angemissiba <as.missiba@b2i-solutions.com>
2023-09-26 03:32:44 +0200

    #v1

---
+++ src/components/blog/BlogBarnerCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const BlogBarnerCard = ({ data }) => {
+    return (
+        <a
+            class="mb-4 md:mb-0 w-full px-5 md:w-full relative rounded inline-block"
+            style={{ height: "24em" }}
+            href="#"
+        >
+            <div class="absolute left-0 bottom-0 w-full h-full z-10"
+                style={{ backgroundImage: `linear-gradient(180deg,transparent,rgba(0,0,0,.7))` }}></div>
+            <img src={data.coverPicture} class="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" />
+            <div class="p-4 absolute bottom-0 left-0 z-20">
+                <span class="px-4 py-1 bg-black text-gray-200 inline-flex datas-center justify-center mb-2">{data.areaPost}</span>
+                <h2 class="text-4xl font-semibold text-gray-100 leading-tight line-clamp-3">
+                    {data.title}
+                </h2>
+                <div class="flex mt-3">
+                    <img src={data.customerPhoto}
+                        class="h-10 w-10 rounded-full mr-2 object-cover" />
+                    <div>
+                        <p class="font-semibold text-gray-200 text-sm"> {data.customerName} </p>
+                        <p class="font-semibold text-gray-400 text-xs"> {moment(data.createdAt).format("DD/MM/YYYY")}</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default BlogBarnerCard
--- src/pages/web/BlogPage.js
+++ src/pages/web/BlogPage.js
@@ -5,6 +5,7 @@ import BlogCard from '../../components/blog/BlogCard';
 import Carousel from 'react-multi-carousel';
 import LoadingBlogBarner from '../../components/loading/LoadingBlogBarner';
 import LoadingBlogContainer from '../../components/loading/LoadingBlogBarner2';
+import BlogBarnerCard from '../../components/blog/BlogBarnerCard';
 
 const BlogPage = () => {
 
@@ -59,7 +60,6 @@ const BlogPage = () => {
                                         })
                                     }
                                 </Carousel>
-
                                 :
                                 error ?
                                     <p>une erreur est survenue</p>
@@ -69,29 +69,7 @@ const BlogPage = () => {
                                             {
                                                 blogs.map((item) => {
                                                     return (
-                                                        <a
-                                                            class="mb-4 md:mb-0 w-full px-5 md:w-full relative rounded inline-block"
-                                                            style={{ height: "24em" }}
-                                                            href="#"
-                                                        >
-                                                            <div class="absolute left-0 bottom-0 w-full h-full z-10"
-                                                                style={{ backgroundImage: `linear-gradient(180deg,transparent,rgba(0,0,0,.7))` }}></div>
-                                                            <img src={item.coverPicture} class="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" />
-                                                            <div class="p-4 absolute bottom-0 left-0 z-20">
-                                                                <span class="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">{item.areaPost}</span>
-                                                                <h2 class="text-4xl font-semibold text-gray-100 leading-tight line-clamp-3">
-                                                                    {item.title}
-                                                                </h2>
-                                                                <div class="flex mt-3">
-                                                                    <img src={item.customerPhoto}
-                                                                        class="h-10 w-10 rounded-full mr-2 object-cover" />
-                                                                    <div>
-                                                                        <p class="font-semibold text-gray-200 text-sm"> {item.customerName} </p>
-                                                                        <p class="font-semibold text-gray-400 text-xs"> {moment(item.createdAt).format("DD/MM/YYYY")}</p>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </a>
+                                                        <BlogBarnerCard data={item} />
                                                     )
                                                 })
                                             }
