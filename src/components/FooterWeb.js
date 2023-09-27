4af200369a71abddce41f84c311517a74ab2d567
angemissiba <as.missiba@b2i-solutions.com>
2023-09-27 12:30:19 +0200

    #v1

--- src/components/FooterWeb.js
+++ src/components/FooterWeb.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { app_bg } from '../utlis/config';
+import {BsLinkedin} from  "react-icons/bs";
+import { routing } from '../utlis/routing';
+
 
 const FooterWeb = () => {
     return (
@@ -10,133 +13,57 @@ const FooterWeb = () => {
                     <div class="grid grid-cols-12 lg:gap-10">
                         <div class="col-span-12 xl:col-span-4">
                             <div class="mr-12">
-                                <h4 class="text-white mb-6 text-[23px]">Jobcy</h4>
+                                <h4 class="text-white mb-6 text-[23px]">Juman</h4>
                                 <p class="text-white/50 dark:text-gray-300">
-                                    It is a long established fact that a reader will be of a page reader
-                                    will be of at its layout.
+                                    Decouvrez le monde de l{"'"}entreprise 
                                 </p>
-                                <p class="mt-3 text-white dark:text-gray-50">Follow Us on:</p>
+                                <p class="mt-3 text-white dark:text-gray-50">Suivez nous sur </p>
                                 <div class="mt-5">
                                     <ul class="flex gap-3">
                                         <li
                                             class="w-8 h-8 leading-loose text-center text-gray-200 transition-all duration-300 border rounded-full cursor-pointer border-gray-200/50 hover:text-gray-50 group-data-[theme-color=violet]:hover:bg-violet-500 group-data-[theme-color=sky]:hover:bg-sky-500 group-data-[theme-color=red]:hover:bg-red-500 group-data-[theme-color=green]:hover:bg-green-500 group-data-[theme-color=pink]:hover:bg-pink-500 group-data-[theme-color=blue]:hover:bg-blue-500 hover:border-transparent">
-                                            <a href="#">
-                                                <i class="uil uil-facebook-f"></i>
+                                            <a href="https://www.linkedin.com/company/uriel-traders/" target='_blank' >
+                                                <BsLinkedin class="h-7 w-7"/>
                                             </a>
                                         </li>
-                                        <li
-                                            class="w-8 h-8 leading-loose text-center text-gray-200 transition-all duration-300 border rounded-full cursor-pointer border-gray-200/50 hover:text-gray-50 group-data-[theme-color=violet]:hover:bg-violet-500 group-data-[theme-color=sky]:hover:bg-sky-500 group-data-[theme-color=red]:hover:bg-red-500 group-data-[theme-color=green]:hover:bg-green-500 group-data-[theme-color=pink]:hover:bg-pink-500 group-data-[theme-color=blue]:hover:bg-blue-500 hover:border-transparent">
-                                            <a href="#">
-                                                <i class="uil uil-linkedin-alt"></i>
-                                            </a>
-                                        </li>
-                                        <li
-                                            class="w-8 h-8 leading-loose text-center text-gray-200 transition-all duration-300 border rounded-full cursor-pointer border-gray-200/50 hover:text-gray-50 group-data-[theme-color=violet]:hover:bg-violet-500 group-data-[theme-color=sky]:hover:bg-sky-500 group-data-[theme-color=red]:hover:bg-red-500 group-data-[theme-color=green]:hover:bg-green-500 group-data-[theme-color=pink]:hover:bg-pink-500 group-data-[theme-color=blue]:hover:bg-blue-500 hover:border-transparent">
-                                            <a href="#">
-                                                <i class="uil uil-google"></i>
-                                            </a>
-                                        </li>
-                                        <li
-                                            class="w-8 h-8 leading-loose text-center text-gray-200 transition-all duration-300 border rounded-full cursor-pointer border-gray-200/50 hover:text-gray-50 group-data-[theme-color=violet]:hover:bg-violet-500 group-data-[theme-color=sky]:hover:bg-sky-500 group-data-[theme-color=red]:hover:bg-red-500 group-data-[theme-color=green]:hover:bg-green-500 group-data-[theme-color=pink]:hover:bg-pink-500 group-data-[theme-color=blue]:hover:bg-blue-500 hover:border-transparent">
-                                            <a href="#">
-                                                <i class="uil uil-twitter"></i>
-                                            </a>
-                                        </li>
+                                        
                                     </ul>
                                 </div>
                             </div>
                         </div>
                         <div class="col-span-12 mt-8 md:col-span-6 xl:col-span-2 md:mt-0">
-                            <p class="mb-6 text-white text-16">Company</p>
+                            <p class="mb-6 text-white text-16">Scoial</p>
                             <ul class="space-y-4">
                                 <li
                                     class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="about.html">
-                                        <i class="mdi mdi-chevron-right"></i> About Us
+                                    <a href={`/${routing.blog_list}`}>
+                                        <i class="mdi mdi-chevron-right"></i> Articles
                                     </a>
                                 </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="contact.html">
-                                        <i class="mdi mdi-chevron-right"></i> Contact Us
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="services.html">
-                                        <i class="mdi mdi-chevron-right"></i> Services
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="blog.html">
-                                        <i class="mdi mdi-chevron-right"></i> Blog
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="team.html">
-                                        <i class="mdi mdi-chevron-right"></i> Team
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="pricing.html">
-                                        <i class="mdi mdi-chevron-right"></i> Pricing
-                                    </a>
-                                </li>
+                                
                             </ul>
                         </div>
                         <div class="col-span-12 mt-8 md:col-span-6 xl:col-span-2 md:mt-0">
-                            <p class="mb-6 text-white text-16">For Jobs</p>
+                            <p class="mb-6 text-white text-16">Jobs</p>
                             <ul class="space-y-4">
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="job-categories.html">
-                                        <i class="mdi mdi-chevron-right"></i> Browser Categories
+                                <li class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
+                                    <a href={`/${routing.job_list}`}>
+                                        <i class="mdi mdi-chevron-right"></i> Offres emplois
                                     </a>
                                 </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="job-list.html">
-                                        <i class="mdi mdi-chevron-right"></i> Browser Jobs
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="job-details.html">
-                                        <i class="mdi mdi-chevron-right"></i> Job Details
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="bookmark-jobs.html">
-                                        <i class="mdi mdi-chevron-right"></i> Bookmark Jobs
-                                    </a>
-                                </li>
+                                
                             </ul>
                         </div>
                         <div class="col-span-12 mt-8 md:col-span-6 xl:col-span-2 md:mt-0">
-                            <p class="mb-6 text-white text-16">For Candidates</p>
+                            <p class="mb-6 text-white text-16">Abonnement</p>
                             <ul class="space-y-4">
                                 <li
                                     class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="candidate-list.html">
-                                        <i class="mdi mdi-chevron-right"></i> Candidate List
+                                    <a href={`/${routing.pricing}`}>
+                                        <i class="mdi mdi-chevron-right"></i> Packs
                                     </a>
                                 </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="candidate-grid.html">
-                                        <i class="mdi mdi-chevron-right"></i> Candidate Grid
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="candidate-details.html">
-                                        <i class="mdi mdi-chevron-right"></i> Candidate Details
-                                    </a>
-                                </li>
+                                
                             </ul>
                         </div>
                         <div class="col-span-12 mt-8 md:col-span-6 xl:col-span-2 md:mt-0">
@@ -144,22 +71,11 @@ const FooterWeb = () => {
                             <ul class="space-y-4">
                                 <li
                                     class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="contact.html">
+                                    <a href="#">
                                         <i class="mdi mdi-chevron-right"></i> Help Center
                                     </a>
                                 </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="faqs.html">
-                                        <i class="mdi mdi-chevron-right"></i> FAQ{"'"}S
-                                    </a>
-                                </li>
-                                <li
-                                    class="text-sm transition-all duration-500 ease-in-out text-white/50 hover:text-gray-50 hover:text-base dark:text-gray-300 dark:hover:text-gray-50">
-                                    <a href="privacy-policy.html">
-                                        <i class="mdi mdi-chevron-right"></i> Privacy Policy
-                                    </a>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
@@ -171,10 +87,9 @@ const FooterWeb = () => {
                     <div class="text-center">
                         <p class="mb-0 text-center text-white/50">
                             <script>document.write(new Date().getFullYear())</script>
-                            © Jobcy - Job Listing Page
-                            Template by
-                            <a href="https://themeforest.net/search/themesdesign" target="_blank"
-                                class="underline transition-all duration-300 hover:text-gray-50">Themesdesign</a>
+                            © Copy Right by
+                            <a href="https://mon-portfolio-mxunivers.web.app" target="_blank"
+                                class="underline transition-all duration-300 hover:text-gray-50"> Dev Aymar Bly</a>
                         </p>
                     </div>
                 </div>
