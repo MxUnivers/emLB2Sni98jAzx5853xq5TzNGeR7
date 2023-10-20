050385964387165fcd85fac283c3c8e99922708c
angemissiba <as.missiba@b2i-solutions.com>
2023-10-20 12:37:03 +0200

    v1

--- lib/src/pages/post_detail_page.dart
+++ lib/src/pages/post_detail_page.dart
@@ -1,16 +1,74 @@
+import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+import "package:intl/intl.dart";
+import "package:jouman_mobile_mobile/src/config/theme.dart";
 
+import "../model/PostModel.dart";
 
 class PostDetailPage extends StatefulWidget {
-  const PostDetailPage({Key? key}) : super(key: key);
+  final PostModel? postdetail;
+
+  const PostDetailPage({Key? key, this.postdetail}) : super(key: key);
 
   @override
   State<PostDetailPage> createState() => _PostDetailPageState();
 }
 
 class _PostDetailPageState extends State<PostDetailPage> {
+  String formatDateTime(String dateTimeString) {
+    final dateTime = DateTime.parse(dateTimeString);
+    final dateFormat = DateFormat.yMMMMd();
+    final timeFormat = DateFormat.Hm();
+
+    final formattedDate = dateFormat.format(dateTime);
+    final formattedTime = timeFormat.format(dateTime);
+
+    return '$formattedDate à $formattedTime';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppTheme_App.withPrimary,
+          elevation: 0.1,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(
+              Icons.chevron_left,
+              color: AppTheme_App.TextGray,
+            ),
+          ),
+          title: Text("${widget.postdetail!.title.toString()}",style: GoogleFonts.nunito(color: AppTheme_App.TextGray) ),
+          actions: [
+            Container(
+              margin: EdgeInsets.only(right: 10),
+            child: IconButton(onPressed: () {}, icon: Icon(Icons.bookmark_add_outlined,color: AppTheme_App.TextGray,))
+          )
+          ],
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+              child: Column(children: [
+            Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: CachedNetworkImage(
+                  imageUrl: widget.postdetail!.coverPicture.toString(),
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                          child: CircularProgressIndicator(
+                              value: downloadProgress.progress)),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  fit: BoxFit.cover,
+                )),
+          ])),
+        ));
   }
 }
--- lib/src/widgets/blog/PostCardItem.dart
+++ lib/src/widgets/blog/PostCardItem.dart
@@ -1,24 +1,21 @@
 import "package:cached_network_image/cached_network_image.dart";
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:intl/intl.dart";
+import "package:jouman_mobile_mobile/src/pages/post_detail_page.dart";
 
 import "../../model/PostModel.dart";
 import 'package:html/parser.dart' show parse;
 
-
-
 class PostCardItem extends StatefulWidget {
-  final PostModel? post ;
-  const PostCardItem({Key? key,this.post}) : super(key: key);
+  final PostModel? post;
+  const PostCardItem({Key? key, this.post}) : super(key: key);
 
   @override
   State<PostCardItem> createState() => _PostCardItemState();
 }
 
 class _PostCardItemState extends State<PostCardItem> {
-
-
-
   String formatDateTime(String dateTimeString) {
     final dateTime = DateTime.parse(dateTimeString);
     final dateFormat = DateFormat.yMMMMd();
@@ -30,89 +27,107 @@ class _PostCardItemState extends State<PostCardItem> {
     return '$formattedDate à $formattedTime';
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return
-      Container(
+    return Container(
         width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.symmetric(vertical: 5,),
-        child: Container(
-            padding: EdgeInsets.only(top: 5,bottom: 15),
-            child: Card(
-              elevation: 0.5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 200,
-                    child:CachedNetworkImage(
-                      imageUrl: widget.post!.coverPicture.toString(),
-                      progressIndicatorBuilder: (context, url, downloadProgress) =>
-                          Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                      fit: BoxFit.cover,
-                    ),
-
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
+        margin: EdgeInsets.symmetric(
+          vertical: 5,
+        ),
+        child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) =>
+                        PostDetailPage(postdetail: widget.post),
+                  ));
+            },
+            child: Container(
+                padding: EdgeInsets.only(top: 5, bottom: 15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Card(
+                    elevation: 0.5,
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.post!.title.toString(),style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
-                        ),),
-                        SizedBox(height: 3,),
-                        Row(
-                          children: [
-                            Text(formatDateTime(widget.post!.createdAt.toString()),style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                                color: Colors.grey
-                            ),),
-                            SizedBox(width: 5,),
-                            Container(
-                              padding: EdgeInsets.only(left:5,right: 5,top: 3,bottom: 3),
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              width: double.infinity,
+                              height: 200,
                               decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(12)
-                              ),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              child: CachedNetworkImage(
+                                imageUrl: widget.post!.coverPicture.toString(),
+                                progressIndicatorBuilder:
+                                    (context, url, downloadProgress) => Center(
+                                        child: CircularProgressIndicator(
+                                            value: downloadProgress.progress)),
+                                errorWidget: (context, url, error) =>
+                                    Icon(Icons.error),
+                                fit: BoxFit.cover,
+                              )),
+                          Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      widget.post!.title.toString(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                    SizedBox(
+                                      height: 3,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          formatDateTime(widget.post!.createdAt
+                                              .toString()),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                              color: Colors.grey),
+                                        ),
+                                        SizedBox(
+                                          width: 5,
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.only(
+                                              left: 5,
+                                              right: 5,
+                                              top: 3,
+                                              bottom: 3),
+                                          decoration: BoxDecoration(
+                                              color: Colors.green,
+                                              borderRadius:
+                                                  BorderRadius.circular(12)),
+                                          child: Text(
+                                            widget.post!.areaPost.toString(),
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 12,
+                                                color: Colors.white),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
 
-                              child: Text(widget.post!.areaPost.toString(),style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12,
-                                  color: Colors.white
-                              ),),
-                            ),
-
-                          ],
-                        ),
-
-                        /*SizedBox(height: 10,),
+                                    /*SizedBox(height: 10,),
                         Text("${widget.post!.content.toString()}",maxLines: 2,style: TextStyle(
                           fontSize: 14,
                         ))*/
-
-                      ]
-                    )
-                  )
-                ]
-              )
-            )
-
-        )
-      );
-    }
+                                  ]))
+                        ])))));
+  }
 
   String extractTextFromHTML(String htmlContent) {
     final document = parse(htmlContent);
     final String text = document.body!.text;
     return text;
   }
-
 }
